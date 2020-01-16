5921d70be53bf4a28b962f3345665e6b6285d63a
hamza <hamza.massaoudi.1994@gmail.com>
2020-01-16 14:50:32 +0100

    add picture .png

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,10 +2,9 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-templates-blog-single-js": () => import("./../src/templates/blog-single.js" /* webpackChunkName: "component---src-templates-blog-single-js" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-blog-js": () => import("./../src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
-  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-templates-blog-single-js": () => import("./../src/templates/blog-single.js" /* webpackChunkName: "component---src-templates-blog-single-js" */)
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
